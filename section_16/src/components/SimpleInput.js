11de6bdcbf3afcf8c4b236e48123ecc0a5760a61
Jason Rabbitt <jrabbitt1110@gmail.com>
2023-05-26 13:36:12 -0600

    Finished Lesson 200 - Adding Basic Validation

--- section_16/src/components/SimpleInput.js
+++ section_16/src/components/SimpleInput.js
@@ -11,11 +11,16 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    if (enteredName.trim() === '') {
+      return;
+    }
+
     console.log(enteredName);
 
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
 
+    // nameInputRef.current.value=''; => NOT IDEAL, DON'T MANIPULATE THE DOM
     setEnteredName('');
   };
 
