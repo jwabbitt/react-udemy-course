0d69e1c6d30ba15805fa13ba3971cda9b53a7199
Jason Rabbitt <jrabbitt1110@gmail.com>
2023-05-30 15:46:54 -0600

    Finished Lesson 217 - Handling Errors

--- section_17/src/components/Meals/AvailableMeals.js
+++ section_17/src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-udemy-181-default-rtdb.firebaseio.com/meals.json'
-      ).then();
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const respnoseData = await response.json();
 
       const loadedMeals = [];
@@ -30,13 +36,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
