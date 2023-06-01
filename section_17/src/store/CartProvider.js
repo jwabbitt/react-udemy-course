a79322f9a53da189374791a011698f86cebe1aac
Jason Rabbitt <jrabbitt1110@gmail.com>
2023-06-01 15:17:42 -0600

    Finished Lesson 222 - Adding Better User Feedback

--- section_17/src/store/CartProvider.js
+++ section_17/src/store/CartProvider.js
@@ -49,10 +49,14 @@ const cartReducer = (state, action) => {
       updatedItems[existingCartItemIndex] = updatedItem;
     }
 
-    return{
+    return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
+  }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -72,11 +76,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
