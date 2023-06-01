792311947b752cad0d8ae1d0270a8eb441c3994f
Jason Rabbitt <jrabbitt1110@gmail.com>
2023-06-01 15:00:16 -0600

    Finished Lesson 221 - Submitting & Sending Cart Data

--- section_17/src/components/Cart/Cart.js
+++ section_17/src/components/Cart/Cart.js
@@ -25,6 +25,15 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch('https://react-udemy-181-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+      orderedItems: cartCtx.items})
+    });
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -60,7 +69,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose}/>}
+      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>}
       {!isCheckout && modalActions}
       
     </Modal>
